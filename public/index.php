a30293bd7a71b05955070353224add95b55fe9d3
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-06 09:31:15 +0000

    Commit

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ ini_set( 'display_errors', 1 );
 ini_set( 'display_startup_errors', 1 );
 error_reporting( E_ALL );
 
-require_once dirname( __DIR__ ) . '/../vendor/autoload_runtime.php';
+require_once dirname( __DIR__ ) . '/vendor/autoload_runtime.php';
 $dotenv = new Dotenv\Dotenv();
 $dotenv->load( dirname( __DIR__ ) );
 
